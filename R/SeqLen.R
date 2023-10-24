eefaca824a242cfdccf6ce3d93c2a8ef361cdfa4
LyroudisK <s232994@dtu.dk>
2023-10-24 09:42:14 +0000

    Fix on function 1 and 2

--- R/SeqLen.R
+++ R/SeqLen.R
@@ -5,7 +5,10 @@
 #' @return creates a nucleotide sequence of given length
 #' @export
 #'
-#'
+#' @examples
+#' my_length <- 10
+#' my_seq <- SeqLen(my_length)
+#' my_seq
 SeqLen <- function(length){
   nucl <- sample(c("A", "T", "G", "C"), size = length, replace = TRUE)
   sequ <- paste0(nucl, collapse = "")
