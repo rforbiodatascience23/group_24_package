4a36ec9e1ca3ef442d4a9307322a1d3251cada47
Morell1123 <s204116@student.dtu.dk>
2023-10-24 07:50:54 +0000

    description of codon table variable added

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Decription of data
+#'
+#' This variable is the Codon Table with all possbile codons and the aninoacids that they translate to
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"CodonTable"
